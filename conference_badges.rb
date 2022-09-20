5cc4440e37a14651881d66258c6d9dc3413c5b8d
ChaseWelsh <fseusb@gmail.com>
2022-09-20 09:28:53 -0600

    commit tree ddde72fe94ebb1ad576a2ebf3e33e0701eca30da

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arr)
+    arr.map { |name| badge_maker(name) }
+end
+
+def assign_rooms(arr)
+    arr.map.with_index do |name, index|
+        "Hello, #{name}! You'll be assigned to room #{index+1}!"
+    end
+end
+
+def printer(arr)
+    batch_badge_creator(arr).map { |out| puts(out) }
+    assign_rooms(arr).map { |out| puts(out) }
+end
